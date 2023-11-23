c47442225dc2e7ccd12aa08d83fd4928a5c40e9c
josevanderley <89553942+josevanderley@users.noreply.github.com>
2023-11-23 11:40:04 -0300

    first commit

---
+++ NotaFiscal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotaFiscal
+{
+    public class NotaFiscal
+    {
+        public int NumeroNota;
+        public DateTime DataEmissao;
+        public string Cliente;
+        public decimal ValorTotal;
+        public bool EstaCancelada;
+
+        public void EmitirNotaFiscal()
+        {
+            Console.WriteLine("Nota fiscal emitida com sucesso.");
+        }
+
+        public void CancelarNotaFiscal()
+        {
+            EstaCancelada = true;
+            Console.WriteLine("Nota fiscal cancelada.");
+        }
+
+        public void CancelarProduto()
+        {
+            Console.WriteLine("Produto cancelado da nota fiscal.");
+        }
+
+        public void AdicionarProduto(string produto, int quantidade, decimal precoUnitario)
+        {
+            Console.WriteLine($"{quantidade}x {produto} adicionado à nota fiscal.");
+            ValorTotal += quantidade * precoUnitario;
+        }
+
+        public void ImprimirDetalhesNota()
+        {
+            Console.WriteLine($"Número da Nota: {NumeroNota}");
+            Console.WriteLine($"Data de Emissão: {DataEmissao}");
+            Console.WriteLine($"Cliente: {Cliente}");
+            Console.WriteLine($"Valor Total: R$ {ValorTotal}");
+            Console.WriteLine($"Status: {(EstaCancelada ? "Cancelada" : "Ativa")}");
+        }
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotaFiscal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NotaFiscal notaFiscal = new NotaFiscal();
+
+            notaFiscal.NumeroNota = 1260;
+            notaFiscal.DataEmissao = DateTime.Now;
+            notaFiscal.Cliente = "Cliente Jose Vanderley";
+
+            notaFiscal.EmitirNotaFiscal();
+
+            notaFiscal.AdicionarProduto("Produto1", 2, 10.50m);
+            notaFiscal.AdicionarProduto("Produto2", 1, 5.75m);
+            notaFiscal.AdicionarProduto("Produto3", 5, 8.55m);
+            
+            notaFiscal.CancelarProduto();
+
+            notaFiscal.ImprimirDetalhesNota();
+
+            notaFiscal.CancelarNotaFiscal();
+
+            notaFiscal.ImprimirDetalhesNota();
+
+            Console.ReadKey();
+        }
+    }
+}
+    
